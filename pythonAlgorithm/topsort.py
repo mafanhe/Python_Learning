a1f81415fb3938a5590de77c0dc0a7e790f90198
mafanhe <827149941@qq.com>
2017-01-02 19:24:26 +0800

    add file

--- pythonAlgorithm/topsort.py
+++ pythonAlgorithm/topsort.py
@@ -2,7 +2,7 @@
 # 拓扑排序
 
 
-# A Naive Algorithm for Topological Sorting
+# Listing 4-9. A Naive Algorithm for Topological Sorting
 def naive_topsort(G, S=None):
 	if S is None: S = set(G)
 	if len(S) == 1: return list(S)
@@ -15,7 +15,26 @@ def naive_topsort(G, S=None):
 	return seq
 
 
+# Listing 4-10. Topological Sorted of a Directed, Acyclic Graph
+def topsort(G):
+	count = dict((u, 0) for u in G)			# The in-degree for each node
+	for u in G:
+		for v in G[u]:
+			count[v] += 1					# Count every in-edge
+	Q = [u for u in G if count[u] == 0]		# Valid initial nodes
+	S = []									# The result
+	while Q:								# While we have start nodes...
+		u = Q.pop()							# Pick one
+		S.append(u)							# Use it as first of the rest
+		for v in G[u]:
+			count[v] -= 1					# "Uncount" its out-edges
+			if count[v] == 0:				# New valid start nodes?
+				Q.append(v)					# Deal with them next
+	return S
+
+
 if __name__ == "__main__":
 	# G = {'a':{'b','f'},'b':{'c','d','f'},'c':{'d'},'d':{'f','e'},'e':{'f'},'f':{}}
 	G = {'b':{'c','d','f'},'a':{'b','f'},'f':{},'d':{'f','e'},'c':{'d'},'e':{'f'}}
 	print naive_topsort(G)
+	print topsort(G)
